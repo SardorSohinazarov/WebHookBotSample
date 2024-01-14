bade43938472f536a30f94c1932ee1b739a71e41
SardorSohinazarov <sardorstudent0618gmail.com>
2024-01-14 12:54:24 +0500

    ConfigureWebhook Service Refactored

--- DemoWebHookBot/Services/ConfigureWebhook.cs
+++ DemoWebHookBot/Services/ConfigureWebhook.cs
@@ -7,32 +7,30 @@ namespace DemoWebHookBot.Services
     public class ConfigureWebhook : BackgroundService
     {
         private readonly ILogger<ConfigureWebhook> _logger;
-        private readonly IServiceProvider _serviceProvider;
         private readonly BotConfiguration _configuration;
+        private readonly ITelegramBotClient _botClient;
 
         public ConfigureWebhook(
             ILogger<ConfigureWebhook> logger,
-            IServiceProvider serviceProvider,
-            IConfiguration configuration)
+            IConfiguration configuration,
+            ITelegramBotClient botClient)
         {
             _logger = logger;
-            _serviceProvider = serviceProvider;
             _configuration = configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
+            _botClient = botClient;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            using var scope = _serviceProvider.CreateScope();
-            var botClient = scope.ServiceProvider.GetRequiredService<ITelegramBotClient>();
             var webhookAddress = $"{_configuration.HostAddress}/bot/{_configuration.Token}";
 
             _logger.LogInformation("Setting webhook");
 
-            await botClient.SendTextMessageAsync(
+            await _botClient.SendTextMessageAsync(
                 chatId: _configuration.MyChatId,
                 text: "Webhook ishlashni boshladi");
 
-            await botClient.SetWebhookAsync(
+            await _botClient.SetWebhookAsync(
                 url: webhookAddress,
                 allowedUpdates: Array.Empty<UpdateType>(),
                 cancellationToken: stoppingToken);
